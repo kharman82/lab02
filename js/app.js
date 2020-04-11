2ec9bb99f42bdb923609daef3e4ed23c63728bda
kharman82 <kharman82@gmail.com>
2020-04-11 14:50:50 -0700

    mustache

--- js/app.js
+++ js/app.js
@@ -11,13 +11,11 @@ let keywordArray = [];
         $.ajax(`./data/${pageNumber}.json`, {method: 'GET', dataType: 'JSON'})
         .then ( (data) => {
             data.forEach( value => {
-                new Monster(value).render();
-                console.log('hello', monstersArray.image_url);
-
+                new Monster(value).renderMonsters(value);
                 if (!keywordArray.includes(value.keyword)){
                     keywordArray.push(value.keyword);
                 }
-                console.log('hello', monstersArray.image_url);
+                // console.log(value);
             });
         populateDropDown();
     });
@@ -29,25 +27,32 @@ function Monster(data) {
     this.image_url = data.image_url;
     this.title = data.title;
     this.description = data.description;
-    this.keyword =data.keyword;
+    this.keyword = data.keyword;
     this.horns = data.horns;
     monstersArray.push(this);
 }
 
 
 // Render the function
-Monster.prototype.render = function() {
-    let template = $('#photo-template').html();
+// Monster.prototype.render = function() {
+//     let template = $('#photo-template').html();
 
-    let $newSection = $('<section></section>');
-    $newSection.html(template);
-    $newSection.find('img').attr('src', this.image_url);
-    $newSection.find('h2').text(this.title);
-    $newSection.find('p').text(this.description);
-    $newSection.attr('keyword', this.keyword);
-    $newSection.attr('horns', this.horns);
+//     let $newSection = $('<section></section>');
+//     $newSection.html(template);
+//     $newSection.find('img').attr('src', this.image_url);
+//     $newSection.find('h2').text(this.title);
+//     $newSection.find('p').text(this.description);
+//     $newSection.attr('keyword', this.keyword);
+//     $newSection.attr('horns', this.horns);
 
-    $('main').append($newSection);
+//     $('main').append($newSection);
+// }
+
+Monster.prototype.renderMonsters = function(data) {
+        let $template = $('#photo-template').html();
+        let $target = $('main');
+        $target.append(Mustache.render($template, data));
+        // console.log('test');
 }
 
 
